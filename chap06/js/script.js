2a4ec5ed581f6ebda62adb390d20883258de0085
노승은 <noseun101@gmail.com>
2021-09-29 22:17:39 +0900

    로또 번호 생성기: 6개 숫자 추출

--- chap06/js/script.js
+++ chap06/js/script.js
@@ -1,17 +1,20 @@
-var list = [];
+let list = [];
 
-for (var i = 1; i <= 45; i++) {
+for (let i = 1; i <= 45; i++) {
     list.push(i); // i만큼 추가
 }
 
-// Math.floor : 소수점 이하를 없앰.
-var index = Math.floor(Math.random() * list.length);
+for (let i = 0; i < 6; i++) {
+    // Math.floor : 소수점 이하를 없앰.
+    let index = Math.floor(Math.random() * list.length);
 
-// 랜덤으로 뽑은 숫자를 배열에 담고 변수 num에 저장함
-var num = list[index];
 
-// splice: 특정 요소를 제거함
-// index 위치에 있는 요소만 제거함.
-list.splice(index, 1);
+    // 랜덤으로 뽑은 숫자를 배열에 담고 변수 num에 저장함
+    let num = list[index];
 
-console.log(num, list);
+    // splice: 특정 요소를 제거함
+    // index 위치에 있는 요소만 제거함.
+    list.splice(index, 1);
+
+    console.log(num, list);
+}
